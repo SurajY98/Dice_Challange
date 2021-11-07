da28f572c7b4fdc0634556766815c089de2e1cc7
Suraj Yadav <78532737+SurajY98@users.noreply.github.com>
2021-11-07 12:43:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "image/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// 2nd dice script
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "image/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+
+// Player 1 win
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins"
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins 🚩"
+}
+else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "😌 Draw 😌"
+}
