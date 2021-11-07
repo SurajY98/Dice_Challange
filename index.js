c213f938f01b5e79c5dbaafe154ab9882d4ab1df
Suraj Yadav <78532737+SurajY98@users.noreply.github.com>
2021-11-07 12:50:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "image/" + randomDiceImage;
+var randomImageSource = "" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -11,7 +11,7 @@ image1.setAttribute("src", randomImageSource);
 // 2nd dice script
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "image/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 var image2 = document.querySelectorAll("img")[1];
 
